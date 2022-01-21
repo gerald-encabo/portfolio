8c4b728cebd6cdf3bb3e162c1ff0fbc83d073292
Gerald Encabo <gerald_encabo@yahoo.com>
2022-01-21 16:47:22 -0500

    initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,10 +11,10 @@ function Footer() {
                 <div className='footer-container container'>
                     <div className='footer-item-left'>
                         <ul>
-                            <li className='footer-item-list'><a className='footer-social-media' href='http://facebook.com/' target='_blank' rel='noreferrer'>Facebook</a></li>
-                            <li className='footer-item-list'><a className='footer-social-media' href='https://www.instagram.com/' target='_blank' rel='noreferrer'>Instagram</a></li>
-                            <li className='footer-item-list'><a className='footer-social-media' href='https://www.linkedin.com/in/gerald-f-encabo/' target='_blank' rel='noreferrer'>LinkedIn</a></li>
-                            <li className='footer-item-list'><a className='footer-social-media' href='https://github.com/geraldencabo' target='_blank' rel='noreferrer'>Github</a></li>
+                            <li className='footer-item-list'><a className='footer-social-media' title="Facebook" href='http://facebook.com/' target='_blank' rel='noreferrer'>Facebook</a></li>
+                            <li className='footer-item-list'><a className='footer-social-media' title="Instagram" href='https://www.instagram.com/' target='_blank' rel='noreferrer'>Instagram</a></li>
+                            <li className='footer-item-list'><a className='footer-social-media' title="LinkedIn" href='https://www.linkedin.com/in/gerald-f-encabo/' target='_blank' rel='noreferrer'>LinkedIn</a></li>
+                            <li className='footer-item-list'><a className='footer-social-media' title="Github" href='https://github.com/geraldencabo' target='_blank' rel='noreferrer'>Github</a></li>
                         </ul>
                     </div>
                     <div className='footer-item-right'>
